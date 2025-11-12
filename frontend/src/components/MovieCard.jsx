989604863c36a34f40eb97b8f05c8ce859e44c34
FarhanDavinRinaldi <farhandavin14@gmail.com>
2025-11-12 09:49:31 +0700

    fix layout

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -2,7 +2,7 @@ import { useMovieContext } from "../contexts/MovieContext";
 import "../css/MovieCard.css";
 
 function Moviecard({ movie }) {
-  // FIX: Mengganti 'addToFavorite' menjadi 'addFavorites'
+
   const { isFavorite, addFavorites, removeFromFavorites } = useMovieContext();
   const favorite = isFavorite(movie.id);
 
@@ -11,34 +11,35 @@ function Moviecard({ movie }) {
     if (favorite) {
       removeFromFavorites(movie.id);
     } else {
-      // FIX: Memanggil fungsi yang benar
+   
       addFavorites(movie);
     }
   }
 
+
   return (
-    <>
-      <div className="movie-card">
-        <div className="movie-poster">
-          <img
-            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-            alt={movie.title}
-          />
-          <div>
-            <button
-              className={`favorite-btn ${favorite ? "active" : ""}`}
-              onClick={onfavoriteClick}
-            >
-              {favorite ? "❤️" : "🤍"}
-            </button>
-          </div>
+    <div className="movie-card"> 
+      <div className="movie-poster">
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+        />
+        <div>
+          <button
+            className={`favorite-btn ${favorite ? "active" : ""}`}
+            onClick={onfavoriteClick}
+          >
+            {favorite ? "❤️" : "🤍"}
+          </button>
         </div>
       </div>
+      
+     
       <div className="movie-info">
         <h3>{movie.title}</h3>
         <p>{movie.release_date?.split("-")[0]}</p>
       </div>
-    </>
+    </div>
   );
 }
 
